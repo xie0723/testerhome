d747f729e934c67dd0c7eb190ac536e09b8c4d68
xie_0723 <xie151007>
2017-03-09 13:23:44 +0800

    添加 article 部分属性

--- testerhome/tscls/article.py
+++ testerhome/tscls/article.py
@@ -9,3 +9,20 @@ class Article(Base):
 	def __init__(self):
 		super(__class__, self).__init__()
 
+	# 获取文章内容
+	def topic(self):
+		pass
+
+	# 获取文章创建时间
+	def topic_time(self):
+		pass
+
+	# 获取文章 标题
+	def topic_title(self):
+		pass
+
+	# 获取文章作者对象
+	def topic_author(self):
+		pass
+
+	#
