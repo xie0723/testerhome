cb5e347ffb29223f014d42d3ee9dfa6c5258b7bb
xie_0723 <xie151007>
2017-04-15 17:08:58 +0800

    update

--- testerhome/tsclient/__init__.py
+++ testerhome/tsclient/__init__.py
@@ -2,6 +2,12 @@
 __Author__ = "xiewm"
 __Date__ = '2017/3/3 14:39'
 
-from testerhome.tsclient import settings
+from .client import TesterHomeClient
+from ..exception import (GetTokenValueFailed, NeedLoginOrUsernameException)
 
-__all__ = ['settings']
+__all__ = {'DOMAIN_URL', 'InsecureRequestWarning', 'LOGIN_URL',
+           'LOGIN_DATA' 'FOLLOWERS_URL', 'FOLLOWING_URL',
+           'FAVORITES_URL', 'ARTICLE_URL', 'GetTokenValueFailed',
+           'LoginTesterHomeFailed', 'login_required', 'NeedLoginOrUsernameException',
+           'GetTokenValueFailed'
+           }
