15903fa180c2f4528bcac1fa9d2e66f92915c90e
xie_0723 <xie151007>
2017-04-15 17:09:28 +0800

    update

--- testerhome/tsclient/client.py
+++ testerhome/tsclient/client.py
@@ -85,7 +85,7 @@ class TesterHomeClient(object):
         """
           :param proxy: :
                  proxies = { "http": "http://10.10.1.10:3128",
-                             "https": "http://10.10.1.10:1080",
+                            "https": "http://10.10.1.10:1080",
                             }
         """
 
