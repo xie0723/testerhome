3272f39c896277bfac27983f92f91c2616b72727
xie0723 <xie_0723@163.com>
2017-04-30 23:16:07 +0800

    添加动态UA

--- testerhome/settings.py
+++ testerhome/settings.py
@@ -1,24 +1,31 @@
 # -*- coding: utf-8 -*-
-__Author__ = "xiewm"
-__Date__ = '2017/3/3 15:04'
+__author__ = "xiewm"
+__date__ = '2017/3/3 15:04'
 
 # 请求头
-HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0',
-    'Accept': '*/*;q=0.5, text/javascript, application/javascript, '
+
+from fake_useragent import FakeUserAgent
+
+def get_user_agent():
+    ua = FakeUserAgent()
+    return ua.random
+
+headers = {
+    'user-agent': get_user_agent(),
+    'accept': '*/*;q=0.5, text/javascript, application/javascript, '
               'application/ecmascript, application/x-ecmascript',
-    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'X-Requested-With': 'XMLHttpRequest',
+    'content-type': 'application/x-www-form-urlencoded; charset=utf-8',
+    'x-requested-with': 'xmlhttprequest',
 }
 
 # 主域名
-DOMAIN_URL = 'https://testerhome.com/'
+domain_url = 'https://testerhome.com/'
 
 # 登录接口
-LOGIN_URL = DOMAIN_URL + 'account/sign_in'
+login_url = domain_url + 'account/sign_in'
 
 # 登录
-LOGIN_DATA = {
+login_data = {
     'utf8': '✓',
     'user[login]': '',
     'user[password]': '',
@@ -27,17 +34,17 @@ LOGIN_DATA = {
 }
 
 # 关注
-FOLLOWERS_URL = DOMAIN_URL + '{}/followers'
+followers_url = domain_url + '{}/followers'
 
 # 正在关注
-FOLLOWING_URL = DOMAIN_URL + '{}/following'
+following_url = domain_url + '{}/following'
 
 # 收藏
-FAVORITES_URL = DOMAIN_URL + '{}/favorites'
+favorites_url = domain_url + '{}/favorites'
 
 # 文章
-ARTICLE_URL = DOMAIN_URL + 'topics/{}'
+article_url = domain_url + 'topics/{}'
 
 # 搜索
-SEARCH_URL = DOMAIN_URL + 'search?{}'
+search_url = domain_url + 'search?{}'
 
